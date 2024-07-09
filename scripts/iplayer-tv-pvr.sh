285c0c78d0ed402e3616f9424b9cc294b4f7ff52
James Barwell <jb@jamesbarwell.co.uk>
2024-07-09 12:31:37 +0100

    Add iPlayer PVR scripts
    - TV and radio have separate config directories as it is seemingly not
      possible for these modes to have their own filename settings

---
+++ scripts/iplayer-tv-pvr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# radio and tv are separate as they could have different configuration
+
+# handy commands
+# list: --pvr-list
+# add: --pvr-add {name} --pid {pid}
+# delete: --pvr-del {name}
+# search: {query}
+# one-off fetch by PID: --pid {pid}
+# one-off fetch by URL: --url {url}
+
+COMMAND=${@}
+
+SCRIPT_DIR=$( dirname -- "${BASH_SOURCE[0]}"; )
+ROOT_DIR=$(realpath ${SCRIPT_DIR}/..)
+
+source ${ROOT_DIR}/config.env
+
+if [ -z "${COMMAND}" ]; then
+    COMMAND="--pvr"
+fi
+
+docker run \
+    --rm \
+    --user $(id -u):$(id -g) \
+    -v ${CONFIG_DIR}/iplayer-tv:/data/config \
+    -v ${IPLAYER_DIR}:/data/output \
+    barwell/get-iplayer:latest \
+    ${COMMAND}
